a8ab34bad554e5d23050d7039e982f5bd4d82ba4
Hüsamettin Demir <65946907+Husam1498@users.noreply.github.com>
2023-08-29 10:14:58 +0300

    Proje dosyası ekle.

---
+++ Form1.cs
@@ -0,0 +1,24 @@
+namespace Artuklu_Billisim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // TextBox nesnesi Oluþturma
+            TextBox textBox1 = new TextBox();
+            textBox1.BackColor = Color.Azure;
+            textBox1.ForeColor = Color.Firebrick;
+            textBox1.Text = "Merhaba, Mesajý buraya yazýn.";
+            textBox1.Name = "dinamikTxt";
+            textBox1.Font = new Font("Georgia", 16);
+
+            Controls.Add(textBox1);
+        }
+    }
+}
